c9a1034022b66eb52c89f7b90d1d3cb2da2bff94
mustafaevf <knetya274@gmail.com>
2022-12-13 01:32:47 +0300

    upd

--- mustafaev/lzss.cpp
+++ mustafaev/lzss.cpp
@@ -10,7 +10,7 @@ struct code {
 	char key;
 	int offset;
 	int length;
-	int type;
+	int prefix;
 };
 
 char dictionary[SIZE_DICTIONARY];
@@ -41,38 +41,22 @@ void lzss_init(std::string text)
 	}
 }
 
-void add_codes(char key, int offset, int length, int type)
+void add_codes(char key, int offset, int length, int prefix)
 {
 	code code;
 	code.key = key;
 	code.offset = offset;
 	code.length = length;
-	code.type = type;
+	code.prefix = prefix;
 	codes.push_back(code);
 }
 
-void print_codes()
-{
-	std::set<char> word;
-	bool f = false;
-	std::cout << "CODES: ";
-	for (auto& k : codes)
-	{
-		if(k.key != ' ')
-			word.insert(k.key);
-	}
-	for (char w : word) {
-		if(w != 0)
-			std::cout << w << ", ";
-	}
-	
-	
-}
+
 
 void search_in_dictionary()
 {
 	int check = 0;
-	for (int i = SIZE_DICTIONARY; i > 0; i--) {
+	for (int i = SIZE_DICTIONARY - 1; i > 0; i--) {
 		if (dictionary[i] == buffer[0])
 		{
 			add_codes(buffer[0], i, 1, 1);
@@ -84,12 +68,8 @@ void search_in_dictionary()
 		add_codes(buffer[0], 0, 0, 0);
 }
 
-void lzss_encode(std::string text)
+std::string lzss_encode(std::string text)
 {
-	std::cout << "ENCODE" << std::endl;
-	std::cout << "TEXT: " << text << std::endl;
-	std::string::size_type sz = sizeof(text);
-	std::cout << "SIZE: " << sz << std::endl;
 	lzss_init(text);
 	int i = 5;
 	bool f = true;
@@ -106,25 +86,51 @@ void lzss_encode(std::string text)
 
 		i++;
 	}
+	std::string result = "";
+	for (auto& k : codes) {
+		if (k.prefix == 0){
+			char ch = k.key;
+			std::string key(1, ch);
+			result += "(" + std::to_string(k.prefix) + "," + key + ")";
+		}
+		if (k.prefix == 1) {
+			result += "(" + std::to_string(k.offset) + "," + std::to_string(k.length) + ")";
+		}
+	}
 	
+	return result;
 }
 
-void lzss_decode()
+std::string lzss_decode(std::string text)
 {
+	codes.clear();
+	int i = 0;
+	while (i < text.size() - 3) {
+		if (text[i] == '(') {
+			if (text[i + 1] == '0') {
+				add_codes(text[i + 3], 0, 0, 0);
+			}
+			if (text[i + 1] != '0') {
+				add_codes('1', text[i + 1] - 48, text[i + 3] - 48, 1);
+			}
+		}
+
+		i++;
+	}
+	std::string result = "";
 	for (int k = 0; k < SIZE_DICTIONARY; k++) 
 	{
 		dictionary[k] = 0;
 	}
-	int i = 0;
-	std::cout << std::endl;
+	i = 0;
 	for (auto& c : codes) {
 		
-		if (c.type == 0) {
-			std::cout << c.key;
+		if (c.prefix == 0) {
+			result += c.key;
 			dictionary[8] = c.key;
 		}
-		if (c.type == 1) {
-			std::cout << dictionary[c.offset - 1];
+		if (c.prefix == 1) {
+			result += dictionary[c.offset - 1];
 			dictionary[8] = dictionary[c.offset - 1];
 		}
 		for (int k = 0; k < SIZE_DICTIONARY - 1; k++) {
@@ -132,14 +138,14 @@ void lzss_decode()
 		}
 		i++;
 	}
+	return result;
 }
 
 int main() 
 {
-	std::string text = "rustam huesos i ebal ego rot";
-	
-	lzss_encode(text);	
-	print_codes();
-	lzss_decode();
+	std::string result_encode = lzss_encode("hfhejfkslkf");
+	std::cout << "ENCODE: " << result_encode << std::endl;
+	std::string reuslt_decode = lzss_decode(result_encode);
+	std::cout << "DECODE: " << reuslt_decode << std::endl;
 	return 0;
 }
--- mustafaev/lzss.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-
-void lzss_encode(std::string text);
-void lzss_decode();
