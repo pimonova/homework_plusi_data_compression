9f679e7090860a05d779de8e753eec53a9b35704
mustafaevf <knetya274@gmail.com>
2022-12-14 13:59:18 +0300

    лзсс обнова

--- mustafaev/LZSS.h
+++ mustafaev/LZSS.h
@@ -1,4 +1,4 @@
+#pragma once
 #include <string>
 
 std::string Mustafaev_lzss_encode(std::string text);
-std::string Mustafaev_lzss_decode(std::string text)
--- mustafaev/lzss.cpp
+++ mustafaev/lzss.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <string>
 #include <set>
-#include "LZSS.h"
+#include "lzss.h"
 
 #define SIZE_DICTIONARY 9
 #define SIZE_BUFFER 5
 
 struct code {
-	char key;
+	std::vector<std::vector<char>> keys;
 	int offset;
 	int length;
 	int prefix;
@@ -19,19 +19,47 @@ char buffer[SIZE_BUFFER];
 
 std::vector<code> codes;
 
-void lzss_move(char sym)
+void lzss_move(std::string text, int offset, int position)
 {
-	for (int i = 0; i < SIZE_DICTIONARY - 1; i++)
+	if (text == "")
 	{
-		dictionary[i] = dictionary[i + 1];
+		for (int i = 0; i < SIZE_DICTIONARY - 1; i++)
+		{
+			dictionary[i] = dictionary[i + 1];
+		}
+		dictionary[SIZE_DICTIONARY - 1] = buffer[0];
+		for (int i = 0; i < SIZE_BUFFER - 1; i++)
+		{
+				buffer[i] = buffer[i + 1];
+			
+		}
+		buffer[SIZE_BUFFER - 1] = 0;
 	}
-	dictionary[SIZE_DICTIONARY - 1] = buffer[0];
-	for (int i = 0; i < SIZE_BUFFER - 1; i++)
-	{
-		buffer[i] = buffer[i + 1];
-	}
+	else {
+		for (int k = 0; k < offset; k++) {
+			for (int i = 0; i < SIZE_DICTIONARY - 1; i++)
+			{
+				dictionary[i] = dictionary[i + 1];
+			}
+			dictionary[SIZE_DICTIONARY - 1] = buffer[0];
+			for (int i = 0; i < SIZE_BUFFER - 1; i++)
+			{
+				
+				buffer[i] = buffer[i + 1];
+			
+			}
 
-	buffer[SIZE_BUFFER - 1] = sym;
+			
+
+			if (text != "" && position + k < text.size())
+				buffer[SIZE_BUFFER - 1] = text[position + k];
+			else
+				buffer[SIZE_BUFFER - 1] = 0;
+		}
+		
+	}
+	
+	
 }
 
 void lzss_init(std::string text)
@@ -42,31 +70,14 @@ void lzss_init(std::string text)
 	}
 }
 
-void add_codes(char key, int offset, int length, int prefix)
+void add_codes(std::vector<char> keys, int offset, int length, int prefix)
 {
 	code code;
-	code.key = key;
+	code.keys.push_back(keys);
 	code.offset = offset;
 	code.length = length;
 	code.prefix = prefix;
 	codes.push_back(code);
-}
-
-
-
-void search_in_dictionary()
-{
-	int check = 0;
-	for (int i = SIZE_DICTIONARY - 1; i > 0; i--) {
-		if (dictionary[i] == buffer[0])
-		{
-			add_codes(buffer[0], i, 1, 1);
-			check++;
-			break;
-		}
-	}
-	if(check == 0)
-		add_codes(buffer[0], 0, 0, 0);
 }
 
 std::string Mustafaev_lzss_encode(std::string text)
@@ -78,24 +89,62 @@ std::string Mustafaev_lzss_encode(std::string text)
 	{
 		if (buffer[0] != 0 && buffer[1] == 0 && buffer[2] == 0)
 			f = false;
-		search_in_dictionary();
 
-		if(i > text.size())
-			lzss_move(0);
-		else 
-			lzss_move(text[i]);	
+		int check = 0;
+		int jq;
+		for (jq = 0; jq < SIZE_DICTIONARY - 3; jq++) {
+			if (dictionary[jq] == buffer[0]) {
+				check++;
+				if (dictionary[jq + 1] == buffer[1]) {
+					check++;
+					if (dictionary[jq + 2] == buffer[2]) {
+						check++;
+						if (dictionary[jq + 3] == buffer[3]) {
+							check++;
+							break;
+						}
+						
+					}
+				}
+			}
+		}
+		if (check == 0) {
+			check = 1;
+			std::vector<char> key;
+			key.push_back(buffer[0]);
+			add_codes(key, jq, 0, 0);
+		}
+		else {
+			
+			std::vector<char> key;
+			key.push_back('0');
+			add_codes(key, jq + 1, check, 1);
+		}
+
+
+		if (i > text.size())
+			lzss_move("", check, i);
+		else
+		{
+			lzss_move(text, check, i);
+			i = i + check - 1;
+		}
+			
 
 		i++;
+		/*if (i > 300) {
+			break;
+		}*/
 	}
 	std::string result = "";
-	for (auto& k : codes) {
-		if (k.prefix == 0){
-			char ch = k.key;
+	for (auto& code : codes) {
+		if (code.prefix == 0) {
+			char ch = code.keys[0][0];
 			std::string key(1, ch);
-			result += "(" + std::to_string(k.prefix) + "," + key + ")";
+			result +=  key;
 		}
-		if (k.prefix == 1) {
-			result += "(" + std::to_string(k.offset) + "," + std::to_string(k.length) + ")";
+		if (code.prefix == 1) {
+			result += "(" + std::to_string(code.offset) + "," + std::to_string(code.length) + ")";
 		}
 	}
 	
@@ -109,10 +158,14 @@ std::string Mustafaev_lzss_decode(std::string text)
 	while (i < text.size() - 3) {
 		if (text[i] == '(') {
 			if (text[i + 1] == '0') {
-				add_codes(text[i + 3], 0, 0, 0);
+				std::vector<char> key;
+				key.push_back(text[i + 3]);
+				add_codes(key, 0, 0, 0);
 			}
 			if (text[i + 1] != '0') {
-				add_codes('1', text[i + 1] - 48, text[i + 3] - 48, 1);
+				std::vector<char> key;
+				key.push_back(' ');
+				add_codes(key, text[i + 1] - 49, text[i + 3] - 48, 1);
 			}
 		}
 
@@ -127,16 +180,29 @@ std::string Mustafaev_lzss_decode(std::string text)
 	for (auto& c : codes) {
 		
 		if (c.prefix == 0) {
-			result += c.key;
-			dictionary[8] = c.key;
+			result += c.keys[0][0];
+			for (int k = 0; k < SIZE_DICTIONARY - 1; k++) {
+				dictionary[k] = dictionary[k + 1];
+			}
+			dictionary[8] = c.keys[0][0];
 		}
 		if (c.prefix == 1) {
-			result += dictionary[c.offset - 1];
-			dictionary[8] = dictionary[c.offset - 1];
+			std::vector<char> bu;
+			for (int j = c.offset; j < c.length; j++) {
+				result += dictionary[j];
+				bu.push_back(dictionary[j]);
+		
+			}
+			for (int q = 0; q < bu.size(); q++) {
+				for (int k = 0; k < SIZE_DICTIONARY - 1; k++) {
+					dictionary[k] = dictionary[k + 1];
+				}
+				dictionary[8] = bu[q];
+			}
+			bu.clear();
 		}
-		for (int k = 0; k < SIZE_DICTIONARY - 1; k++) {
-			dictionary[k] = dictionary[k + 1];
-		}
+		
+		/*dictionary[8] = '0';*/
 		i++;
 	}
 	return result;
